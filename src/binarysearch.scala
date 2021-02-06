7e032738d4f7b6edc3cc33432ba384b0e3ffa185
Gennady Petrov <brundrom@gmail.com>
2021-02-06 20:35:09 +0300

    Added index and total iterations count

--- src/binarysearch.scala
+++ src/binarysearch.scala
@@ -6,13 +6,14 @@ object binarysearch extends App {
   val conList = (0 to 100).toList
 
   @tailrec
-  def binsearch3(conList: List[Int], low: Int, high: Int, find: Int): Int = {
+  def binsearch3(conList: List[Int], low: Int, high: Int, find: Int, iterato: Int): (Int, Int) = {
     val med = (low + high) / 2
     val maybe = conList(med)
-    if(maybe == find) med
-    else if(maybe > find) binsearch3(conList, low, med + 1, find)
-    else binsearch3(conList, med - 1, high, find)
+    if(maybe == find) (med, iterato)
+    else if(maybe > find) binsearch3(conList, low, med + 1, find, iterato + 1)
+    else binsearch3(conList, med - 1, high, find, iterato + 1)
   }
 
-  println(binsearch3(conList, 0, conList.length - 1, 32))
+  val(index, iterat) = binsearch3(conList, 0, conList.length - 1, 30, 0)
+  println(s"Finded digit: $index by total iterations: $iterat")
 }
