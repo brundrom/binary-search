d7cc0a903e567e9bd269ce8ee5d4ec41df441eef
Gennady Petrov <brundrom@gmail.com>
2021-02-06 12:40:38 +0300

    Added binary search

--- src/binarysearch.scala
+++ src/binarysearch.scala
@@ -5,17 +5,22 @@ object binarysearch extends App {
   val conList = new Array[Int](max)
   for(a <- 0 until max) {
     conList(a) = a
-    println(a)
   }
 
   def binsearch(digit: Int): Unit = {
-    var i = 0
-    while (i <= digit) {
-      println(i)
-      i += 1
+    var iteration = 0
+    var low = 0
+    var high = max - 1
+    while (low <= high) {
+      iteration += 1
+      val med = low + high
+      var maybe = conList(med)
+      if(maybe == digit) println(iteration) //can change to iteration var
+      if(maybe > digit ) high = med - 1
+      else low = med + 1
     }
   }
 
-  //binsearch(56)
+  binsearch(89)
 
 }
