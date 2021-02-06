f11599930e09993f47932d50e2afb060f20bbef2
Gennady Petrov <brundrom@gmail.com>
2021-02-06 12:10:50 +0300

    Fixed cycle.

--- src/binarysearch.scala
+++ src/binarysearch.scala
@@ -3,16 +3,15 @@ object binarysearch extends App {
 
   val max = 100
 
+  //val conList = new Array[Int](100)
+
   def binsearch(digit: Int): Unit = {
     var i = 0
     while (i <= digit) {
-      if(digit == max)
       println(i)
       i += 1
     }
   }
-
-
 
   binsearch(56)
 
