b046fce0150974aa749f3948c50a7206d10b1039
Gennady Petrov <brundrom@gmail.com>
2021-02-06 13:27:39 +0300

    Fixed binary search

--- src/binarysearch.scala
+++ src/binarysearch.scala
@@ -7,20 +7,20 @@ object binarysearch extends App {
     conList(a) = a
   }
 
-  def binsearch(digit: Int): Unit = {
+  def binsearch2(digit: Int): Unit = {
     var iteration = 0
     var low = 0
     var high = max - 1
     while (low <= high) {
       iteration += 1
-      val med = low + high
-      var maybe = conList(med)
+      val med = (low + high) / 2
+      val maybe = conList(med)
       if(maybe == digit) println(iteration) //can change to iteration var
       if(maybe > digit ) high = med - 1
       else low = med + 1
     }
   }
 
-  binsearch(89)
+  binsearch2(68)
 
 }
